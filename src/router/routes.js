ba4510b304974c7996454fe0c64922dcdcaefad1
AlejandroOrdenes <alejandro.ordenes.o@gmail.com>
2023-01-10 11:17:19 -0300

    loginRoute

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,12 +1,13 @@
 
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: '/', component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: '/login', component: () => import('pages/LoginPage.vue') }
+      { path: 'home', component: () => import('pages/IndexPage.vue') }
     ]
+  },
+  {
+    path: '/login', component: () => import('pages/LoginPage.vue')  
   },
 
   // Always leave this as last one,
