52ec4c7ad6b6d1ba0b0fb83c8cc0666058639d16
AlejandroOrdenes <alejandro.ordenes.o@gmail.com>
2023-01-11 13:44:48 -0300

    loginFunc

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,72 +1,102 @@
 <template>
-    <div class="q-pa-md" style="
+  <div
+    class="q-pa-md"
+    style="
       width: 100%;
       align-items: center;
       display: flex;
       align-items: center;
       flex-direction: column;
-    ">
-        <div class="q-mt-md q-gutter-md">
-            <q-icon style="padding-bottom: 60px; padding-top: 30px" name="map" color="blue" size="7.4em" />
-        </div>
-
-        <q-form style="width: 75%" @submit="onSubmit" class="q-gutter-md">
-            <q-input filled type="text" v-model="user" label="Usuario *" lazy-rules
-                :rules="[(val) => (val && val.length > 0) || 'Ingrese usuario valido']" />
-
-            <q-input filled type="number" v-model="pass" label="Password *" lazy-rules :rules="[
-                (val) => (val !== null && val !== '') || 'Ingrese password valido',
-            ]" />
-
-            <div style="justify-content: center; display: flex; padding-top: 20px">
-                <q-btn label="Ingresar" type="submit" color="primary" />
-            </div>
-        </q-form>
+    "
+  >
+    <div class="q-mt-md q-gutter-md">
+      <q-icon
+        style="padding-bottom: 60px; padding-top: 30px"
+        name="map"
+        color="blue"
+        size="7.4em"
+      />
     </div>
+
+    <q-form style="width: 75%" @submit="login" class="q-gutter-md">
+      <q-input
+        filled
+        type="text"
+        v-model="email"
+        label="Usuario *"
+        lazy-rules
+        :rules="[(val) => (val && val.length > 0) || 'Ingrese usuario valido']"
+      />
+
+      <q-input
+        filled
+        type="text"
+        v-model="password"
+        label="Password *"
+        lazy-rules
+        :rules="[
+          (val) => (val !== null && val !== '') || 'Ingrese password valido',
+        ]"
+      />
+
+      <div style="justify-content: center; display: flex; padding-top: 20px">
+        <q-btn type="submit" label="Ingresar" color="primary" />
+      </div>
+    </q-form>
+    <div
+      class="q-mt-md q-gutter-md"
+      style="display: flex; align-items: center; flex-direction: column"
+    >
+      <a href="" style="text-decoration: none">Crear cuenta</a>
+      <a href="" style="text-decoration: none">Restablecer contraseña</a>
+    </div>
+  </div>
 </template>
 
 <script>
 import { useQuasar } from "quasar";
 import { ref } from "vue";
-import { useRouter } from 'vue-router'
+import { useRouter } from "vue-router";
 
 export default {
-    setup() {
-        const $q = useQuasar();
-        const router = useRouter();
-        const user = ref(null);
-        const pass = ref(null);
+  data() {
+    return {
+      $q: useQuasar(),
+      router: useRouter(),
+      email: ref(""),
+      password: ref(""),
+      
+    };
+  },
 
-        return {
-            user,
-            pass,
-
-            onSubmit() {
-                if (user.value == null || pass.value == null) {
-                    $q.notify({
-                        color: "red-7",
-                        textColor: "white",
-                        icon: "warning",
-                        message: "Usuario y/o password incorrecto",
-                    });
-
-                    
-                } else {
-                    $q.notify({
-                        color: "green-7",
-                        textColor: "white",
-                        icon: "cloud_done",
-                        message: "Login correcto",
-                    });
-                    router.push('/home')
-                }
-            },
-
-            onReset() {
-                user.value = null;
-                pass.value = null;
-            },
-        };
+  methods: {
+    async login() {
+      const data = {
+        email: this.email,
+        password: this.password,
+      };
+      await this.$axios
+        .post("http://localhost:3003/api/login", data)
+        .then((response) => {
+          console.log(response);
+          this.$q.notify({
+            color: "green-7",
+            textColor: "white",
+            icon: "cloud_done",
+            message: "Login correcto",
+          });
+          this.router.push("/home");
+        })
+        .catch((err) => {
+          console.log(err);
+          this.$q.notify({
+            color: "red-7",
+            textColor: "white",
+            icon: "warning",
+            message: "Usuario y/o password incorrecto",
+          });
+        });
     },
+  },
 };
 </script>
--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,19 +1,60 @@
-import { authStore } from 'pinia';
+import { defineStore } from 'pinia';
 
-export const useCounterStore = defineStore('counter', {
+export const useUserStore = defineStore('user', {
   state: () => ({
-    counter: 0
+    id: null,
+    firstName: null,
+    lastName: null,
+    email: null
   }),
 
   getters: {
-    doubleCount (state) {
-      return state.counter * 2
-    }
+    getId: (state) => state.id,
+    getFirstName: (state) => state.firstName,
+    getLastName: (state) => state.lastName,
+    getEmail: (state) => state.email,
   },
 
   actions: {
-    increment () {
-      this.counter++
+
+     async login (email, password) {
+      const data = {
+        email: email,
+        password: password
+      }
+        await this.$axios.post("http://localhost:3003/api/login", data).then((response) => {
+          console.log(response);
+          alert("Datos correctos");
+        })
+        .catch((err) => {
+          console.log(err);
+          console.log("Error al obtener datos!");
+        });
+
+    },
+
+    // async fetchUser () {
+    //   try {
+    //     return await this.$axios.get("http://localhost:3003/api/user")
+    //   } catch (error) {
+    //     if (error) throw error
+    //   }
+    // },
+
+
+    setUser (payload) {
+      if (payload.id) this.id = payload.id
+      if (payload.firstName) this.id = payload.firstName
+      if (payload.lastName) this.id = payload.lastName
+      if (payload.email) this.id = payload.email
+
+    },
+
+    clearUser () {
+      this.id = null,
+      this.lastName = null,
+      this.firstName = null,
+      this.email = null
     }
   }
 })
